be17aebbfb43082687d7b9509948c3d12dbbcdcb
J B <jayanthi.bsr@gmail.com>
2025-07-01 21:17:13 +1000

    two sum another approach

--- Arrays/twosum.py
+++ Arrays/twosum.py
@@ -1,3 +1,4 @@
+#brute force
 def two_sum(nums, target):
     target_list = []
     for i in range(len(nums)-1):
@@ -7,5 +8,23 @@ def two_sum(nums, target):
                 target_list.append(j)
     return target_list
 
+
 print(two_sum([2,7,11,15], 9))
 
+#Hash map
+
+def two_sum_using_map(nums, target):
+    target_map = {}
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        if diff in target_map:
+            return (target_map[diff], i)
+        target_map[nums[i]] = i
+    return []
+
+print(two_sum_using_map([2, 7, 11,15], 9))
+
+#Approach
+# 1. Iterate through each element
+# 2. Store the index of the element for each element in a hash map
+# 3. check if the difference of element is in the map, if so return the index of the current element and the index of the difference
